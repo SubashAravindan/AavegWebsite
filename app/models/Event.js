b443422cd2061f16b997eb38a110e1fe9f412050
SubashAravindan <p.subasharavindan@gmail.com>
2018-11-25 01:13:52 +0530

    Refactor: Edit winners, points in event schema

--- app/models/Event.js
+++ app/models/Event.js
@@ -3,25 +3,12 @@ const mongoose = require('mongoose')
 const eventSchema = new mongoose.Schema({
   name: 'string',
   cup: 'string',
-  points: {
-    first: 'number',
-    second: 'number',
-    third: 'number'
-  },
-  firstPlace: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Hostel'
-  }
-  ],
-  secondPlace: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Hostel'
-  }
-  ],
-  thirdPlace: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Hostel'
-  }
+  points: ['number'],
+  winners: [
+    [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Hostel'
+    }]
   ],
   venue: {
     type: mongoose.Schema.Types.ObjectId,
