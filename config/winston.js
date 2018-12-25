5b5797317f577fb481c281dd5f062169cde85742
abishake <abishake.110899@gmail.com>
2018-12-25 20:08:47 +0530

    Fix: Urgent fixes in template
    - Fix undefined #
    - Some style changes
    - Standard js fix on winston.js package.json
    - Add links in template

--- config/winston.js
+++ config/winston.js
@@ -1,16 +1,16 @@
 const winston = require('winston')
 const path = require('path')
 const rootDir = path.parse(__dirname).dir
-console.log(rootDir);
+console.log(rootDir)
 
 module.exports = winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.simple(),
-    winston.format.printf(msg => 
-      winston.format.colorize().colorize(msg.level, JSON.stringify({level:msg.level, timestamp:msg.timestamp, message:msg.message}))
+    winston.format.printf(msg =>
+      winston.format.colorize().colorize(msg.level, JSON.stringify({ level: msg.level, timestamp: msg.timestamp, message: msg.message }))
     )
-    ),
+  ),
   transports: [
     new winston.transports.Console({ level: 'silly' }),
     new winston.transports.File({ filename: `${rootDir}/logs/errors.log`, level: 'error' }),
