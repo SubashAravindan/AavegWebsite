ab24a73f95a04def01b8ff3aad18e7724fdfa491
ishanisri <ishasri.1212@gmail.com>
2019-01-09 17:54:49 +0530

    Sorting added to total scores

--- app/controllers/adminScoreboardController.js
+++ app/controllers/adminScoreboardController.js
@@ -16,31 +16,29 @@ exports.getPoints = async (req, res) => {
   return res.status(200).send(event.points)
 }
 
-
-exports.validate=[
+exports.validate = [
   check('eventId')
-  .exists().withMessage('Event missing')
-  .custom(async (eventId) => {
-    const eventData = await Events.find({}).exec()
-    const eventIdList = eventData.map(eventId => eventData._id)
-    if (!eventIdList.includes(eventId)) {
-      throw new Error('Event data incorrect')
-    } else {
-      return true
-    }
-  }),
+    .exists().withMessage('Event missing')
+    .custom(async (eventId) => {
+      const eventData = await Events.find({}).exec()
+      const eventIdList = eventData.map(eventId => eventData._id)
+      if (!eventIdList.includes(eventId)) {
+        throw new Error('Event data incorrect')
+      } else {
+        return true
+      }
+    }),
   check('position*').exists().withMessage('Hostel missing')
-  .custom(async (hostelId)=>{
-    const hostelData=await Hostels.find({}).exec()
-   const hostelIdList=hostelData.map(hostelId=>hostelData._id)
-    if(!hostelIdList.includes(hostelId)){
-      throw new Error('Hostel data incorrect')
-    } else {
-      return true
-    }
-  }),
-  check('points*','It should be a whole number').isInt()
-
+    .custom(async (hostelId) => {
+      const hostelData = await Hostels.find({}).exec()
+      const hostelIdList = hostelData.map(hostelId => hostelData._id)
+      if (!hostelIdList.includes(hostelId)) {
+        throw new Error('Hostel data incorrect')
+      } else {
+        return true
+      }
+    }),
+  check('points*', 'It should be a whole number').isInt()
 
 ]
 
@@ -55,34 +53,34 @@ exports.createScore = async (req, res) => {
       error: errorMessages
     })
   } else {
-  let noOfPositions = []//array of hostels at different positions with keys position1, position2... in the body's object
-  let noOfPoints = []//array of points of different positions with keys points1,points2.... in the body's object
-  let keys = Object.keys(req.body)
-  keys.forEach(function (item) {
-    if (item.indexOf('position') !== -1) {
-      noOfPositions.push(req.body[item])//items entered into the array after checking the key
-    }
-    if (item.indexOf('points') !== -1) { noOfPoints.push(req.body[item]) }//items added into the array after checking the key
-  })
-  for (let j = 0; j < noOfPositions.length; j++) {
-    let hostelList = noOfPositions[j]//get hostels at a particular position
-    let points = noOfPoints[j]//get points at a particular position
-    for (let i = 0; i < hostelList.length; i++) {
-      let pos = new Score({
-        hostel: hostelList[i],
-        event:req.body.eventId,
-        position: (j + 1),
-        points: points
-      })
-      try {
-        let savedPos = await pos.save()
-        logger.info(`Scores of ${savedPos._id} added by ${req.session.rollnumber}`)
-      } catch (error) {
-        logger.error(error)
-        return res.status(500).send(error)
+    let noOfPositions = []// array of hostels at different positions with keys position1, position2... in the body's object
+    let noOfPoints = []// array of points of different positions with keys points1,points2.... in the body's object
+    let keys = Object.keys(req.body)
+    keys.forEach(function (item) {
+      if (item.indexOf('position') !== -1) {
+        noOfPositions.push(req.body[item])// items entered into the array after checking the key
+      }
+      if (item.indexOf('points') !== -1) { noOfPoints.push(req.body[item]) }// items added into the array after checking the key
+    })
+    for (let j = 0; j < noOfPositions.length; j++) {
+      let hostelList = noOfPositions[j]// get hostels at a particular position
+      let points = noOfPoints[j]// get points at a particular position
+      for (let i = 0; i < hostelList.length; i++) {
+        let pos = new Score({
+          hostel: hostelList[i],
+          event: req.body.eventId,
+          position: (j + 1),
+          points: points
+        })
+        try {
+          let savedPos = await pos.save()
+          logger.info(`Scores of ${savedPos._id} added by ${req.session.rollnumber}`)
+        } catch (error) {
+          logger.error(error)
+          return res.status(500).send(error)
+        }
       }
     }
+    res.redirect('/admin')
   }
-  res.redirect('/admin')
-}
 }
--- app/routes/scoreboard.js
+++ app/routes/scoreboard.js
@@ -4,7 +4,7 @@ const scoreController = require('../controllers/scoreboardController.js')
 const adminScoreboardController = require('../controllers/adminScoreboardController.js')
 const adminAuthController = require('../controllers/adminAuthController.js')
 
-router.get('/admin/scoreboard',adminAuthController.checkAdminLogin, adminScoreboardController.showScoreForm)
+router.get('/admin/scoreboard', adminAuthController.checkAdminLogin, adminScoreboardController.showScoreForm)
 router.post('/admin/scoreboard', adminAuthController.checkAdminLogin, adminScoreboardController.createScore)
 router.get('/api/scoreboard', scoreController.showScoreboard)
 router.get('/getPoints', adminScoreboardController.getPoints)
