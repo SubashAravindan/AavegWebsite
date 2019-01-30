a8cc3d4e1c75c5988096ec04a9623113a212ea47
SubashAravindan <p.subasharavindan@gmail.com>
2019-01-30 16:59:18 +0530

    Feat:schedule

--- app/routes/misc.js
+++ app/routes/misc.js
@@ -5,6 +5,10 @@ router.get('/about', (req, res) => {
   res.render('about', { title: 'About Aaveg' })
 })
 
+router.get('/schedule', (req, res) => {
+  res.download('public/others/Schedule.pdf', 'Schedule.pdf')
+})
+
 router.get('*', (req, res) => {
   res.render('comingSoon', { url: req.url })
 })
